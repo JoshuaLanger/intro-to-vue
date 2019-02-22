4654c96620bca204b42b7e057af11d26181416d4
Joshua Langer <joshuarllanger@gmail.com>
2019-02-22 10:26:32 -0600

    added assets and v-binds

--- main.js
+++ main.js
@@ -2,6 +2,9 @@ var app = new Vue({
   el: '#app',
   data: {
     product: 'Socks',
-    desc: `The best socks you'll find in town!`
+    desc: `The best socks you'll find in town!`,
+    image: './socks_green.jpeg',
+    alt: 'Green socks',
+    link: 'https://bombas.com/collections/mens-socks'
   }
 });
