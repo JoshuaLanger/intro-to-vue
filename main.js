e5aabd3cf201157c309d229c35d58c50151323e8
Joshua Langer <joshuarllanger@gmail.com>
2019-02-22 11:11:28 -0600

    event handling

--- main.js
+++ main.js
@@ -6,19 +6,37 @@ var app = new Vue({
     image: './socks_green.jpeg',
     alt: 'Green socks',
     link: 'https://bombas.com/collections/mens-socks',
-    inventory: 100,
+    inventory: 20,
     onSale: true,
     details: ['80% cotton', '20% polyester', '100% comfort'],
     sizes: ['S', 'M', 'L', 'XL'],
     variants: [
       {
         variantId: 234,
-        variantColor: 'Green'
+        variantColor: 'Green',
+        variantImage: './socks_green.jpeg'
       },
       {
         variantId: 235,
-        variantColor: 'Blue'
+        variantColor: 'Blue',
+        variantImage: './socks__blue.jpeg'
       }
-    ]
+    ],
+    cart: 0
+  },
+  methods: {
+    addToCart: function() {
+      if (this.inventory === 0) return this.cart;
+      this.cart += 1;
+      this.inventory -= 1;
+    },
+    removeFromCart: function() {
+      if (this.cart === 0) return 0;
+      this.cart -= 1;
+      this.inventory += 1;
+    },
+    updateProduct: function(variantImage) {
+      this.image = variantImage;
+    }
   }
 });
